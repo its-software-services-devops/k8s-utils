ed50e2e20aea97c670a945d3d66dd5a343f7a242
Seubpong Monsar <pjame.fb@gmail.com>
2021-06-01 13:04:38 +0700

    Dump filtered file from yq

--- k8s-utils/Its/K8SUtils/Processors/Snapshoters/ResourcesSnapshoter.cs
+++ k8s-utils/Its/K8SUtils/Processors/Snapshoters/ResourcesSnapshoter.cs
@@ -8,7 +8,22 @@ namespace Its.K8SUtils.Processors.Snapshoters
 {
     public class ResourcesSnapshoter : BaseProcessor
     {
+        private List<string> excludedList = new List<string>() 
+        { 
+            ".items.[].metadata.managedFields", 
+            ".items.[].status",
+            ".items.[].metadata.creationTimestamp",
+            ".items.[].metadata.generation",
+            ".items.[].metadata.selfLink",
+            ".items.[].metadata.resourceVersion",
+            ".items.[].metadata.uid",
+            ".items.[].metadata.annotations.\"kubectl.kubernetes.io/last-applied-configuration\"",
+        };
+
         private readonly string tmpFile = Path.GetTempFileName();
+
+        private readonly string filterCmd = "yq";
+        private readonly string filterArgv = "e 'del({0})' {1}";
 
         private readonly string cmdStr = "kubectl";
         private readonly string getResSubCmd = "api-resources -o name --namespaced={0}";
@@ -35,13 +50,19 @@ namespace Its.K8SUtils.Processors.Snapshoters
             string gbLevelName = String.Format("{0}_global.yaml", tmpFile);
             string nsLevelName = String.Format("{0}_ns.yaml", tmpFile);
 
-            using StreamWriter gbFile = new(gbLevelName);
-            gbFile.WriteLine(glbRes);
+            string gbFilterName = String.Format("{0}/global_resources.yaml", opt.ExportOutputDir);
+            string nsFilterName = String.Format("{0}/ns_resources.yaml", opt.ExportOutputDir);
 
-            using StreamWriter nsFile = new(nsLevelName);
-            nsFile.WriteLine(nsRes);
+            File.WriteAllText(gbLevelName, glbRes);
+            File.WriteAllText(nsLevelName, nsRes);
 
-            Log.Information("Wrote file [{0}] and [{1}]", gbLevelName, nsLevelName);
+            string glbFilterCtn = GetFilteredContent(gbLevelName);
+            string nsFilterCtn = GetFilteredContent(nsLevelName);
+
+            File.WriteAllText(gbFilterName, glbFilterCtn);
+            File.WriteAllText(nsFilterName, nsFilterCtn);
+
+            Log.Information("Wrote file [{0}] and [{1}]", gbFilterName, nsFilterName);
         }
 
         private string GetResources(List<string> kinds, string subCmd)
@@ -62,6 +83,16 @@ namespace Its.K8SUtils.Processors.Snapshoters
             var arr = Utils.Utils.StringsToArray(result);
 
             return arr;
+        }
+
+        private string GetFilteredContent(string fName)
+        {
+            string excList = String.Join(',', excludedList);
+
+            string argv = String.Format(filterArgv, excList, fName);
+            string result = executor.Run(filterCmd, argv);
+
+            return result;
         }
     }
 }
